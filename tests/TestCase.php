871d4d703a0e6a4893ad3f629833aded3a955717
Balsakup <balsakup@gmail.com>
2021-02-25 17:39:42 +0100

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,40 +2,15 @@
 
 namespace Kapouet\Klaro\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Kapouet\Klaro\KlaroServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Kapouet\\Klaro\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             KlaroServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_laravel_klaro_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }
